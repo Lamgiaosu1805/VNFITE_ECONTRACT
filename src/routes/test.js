a13098a755a84fe99cb2d1f4c5c1ed50532536d2
Lamgs <nghiemkhaclam185.mta@gmail.com>
2025-05-14 11:32:33 +0700

    update

--- src/routes/test.js
+++ src/routes/test.js
@@ -4,6 +4,6 @@ const auth = require('../middlewares/auth');
 const router = express.Router()
 
 router.get('/', TestController.index);
-router.get('/ssoCallback', auth.verifyToken, TestController.ssoCallback);
+router.post('/ssoCallback', auth.verifyToken, TestController.ssoCallback);
 
 module.exports = router;

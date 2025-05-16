5ef7ec1e175e94b6a04cfa58dcee62345cdd1c45
Lamgs <nghiemkhaclam185.mta@gmail.com>
2025-05-16 15:59:43 +0700

    clear log

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -1,20 +1,8 @@
 const jwt = require('jsonwebtoken')
 const bcrypt = require('bcrypt')
 const PartnerModel = require('../models/PartnerModel')
-// const { FailureResponse } = require('../utils/ResponseRequest')
-// const AdminAccountModel = require('../models/AdminAccountModel')
-// const CustomerModel = require('../models/CustomerModel')
 
 const auth = {
-    // verifyTokenAdmin: (req, res, next) => {
-    //     // verifyToken(true, req, res, next, false)
-    // },
-    // verifyTokenCustomerNonEkyc: (req, res, next) => {
-    //     // verifyToken(false, req, res, next, false)
-    // },
-    // verifyTokenCustomer: (req, res, next) => {
-    //     // verifyToken(false, req, res, next, true)
-    // },
     verifyPartner: async (req, res, next) => {
         const token = req.headers.authorization
         if(token) {
@@ -70,41 +58,5 @@ const auth = {
         }
     }
 }
-
-// const verifyToken = (isAdmin, req, res, next, requireEkyc) => {
-//     const token = req.headers.authorization;
-//     if(token) {
-//         const accessToken = token.split(" ")[1];
-//         jwt.verify(accessToken, isAdmin == true ? process.env.SECRET_KEY_QT : process.env.SECRET_KEY, async (err, user) => {
-//             if(err) {
-//                 console.log(err)
-//                 res.json(FailureResponse('20', err))
-//             }
-//             else {
-//                 req.user = user;
-//                 req.isAdmin = isAdmin;
-//                 try {
-//                     var validatedUser
-//                     isAdmin == true ? validatedUser = await AdminAccountModel.findById(user.id) : validatedUser = await CustomerModel.findById(user.id)
-//                     if(!isAdmin && validatedUser.isEkyc == false && requireEkyc) {
-//                         return res.json(FailureResponse("28"))
-//                     }
-//                     if(!validatedUser?.isDelete) {
-//                         next();
-//                     }
-//                     else {
-//                         res.json(FailureResponse("04"))
-//                     }
-//                 } catch (error) {
-//                     console.log(error)
-//                     res.json(FailureResponse("21", error))
-//                 }
-//             }
-//         })
-//     } else {
-//         res.json(FailureResponse('22'))
-//         console.log("Not Authenticated")
-//     }
-// }
 
 module.exports = auth

c7df248f2628c95cba26813f585a1ab544d607fa
vuvinh <vuthanhvinh0112@gmail.com>
2025-05-21 11:29:05 +0700

    update

--- src/routes/hopDong.js
+++ src/routes/hopDong.js
@@ -3,8 +3,9 @@ const HopDongController = require('../controllers/HopDongController');
 const auth = require('../middlewares/auth');
 const ipFilterMiddleware = require('../middlewares/ipWhitelist');
 const router = express.Router();
+const LoaiHopDongController = require('../controllers/LoaiHopDongController')
 const { createLoaiHopDong } = require('../controllers/LoaiHopDongController');
 router.post('/renderHDPreview', auth.verifyTokenPartner, HopDongController.renderHD);
 router.get('/chiTietHopHong/:templateId', auth.verifyTokenPartner, HopDongController.getChiTietHD);
-router.post('/loaiHopDong', createLoaiHopDong);
+router.post('/loaiHopDong', LoaiHopDongController.createLoaiHopDong);
 module.exports = router;

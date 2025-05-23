d532cc770421f489bc47160b109cef1d11a6000e
Lamgs <nghiemkhaclam185.mta@gmail.com>
2025-05-23 13:22:58 +0700

    create update token third party

--- src/jobs/updateTokenThirdParty.js
+++ src/jobs/updateTokenThirdParty.js
@@ -1,3 +1,16 @@
+const cron = require('node-cron');
+const getTokenVnpt = require('../utils/getTokenVnpt');
+const redis = require('../config/connectRedis');
+
 module.exports = () => {
-    
+    cron.schedule('*/10 * * * *', async () => {
+        try {
+            const data = await getTokenVnpt.tokenThirdParty()
+            await redis.set('tokenThirdPartyVnpt:TIKLUY', data.access_token, "EX", 3600 * 23)
+            console.log("JOB Update Token Third Party VNPT TIKLUY: Success")
+        } catch (error) {
+            console.log("JOB Update Token Third Party VNPT TIKLUY Failure")
+            console.log(error)
+        }
+    }) 
 }

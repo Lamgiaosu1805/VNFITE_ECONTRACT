9ee084fdd3e29344f468ca7ed7d3e445af84d4a7
Lamgs <nghiemkhaclam185.mta@gmail.com>
2025-05-24 09:15:25 +0700

    update limit body

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ require('dotenv').config();
 
 //use middlewares
 app.use(morgan('dev'))
-app.use(express.json())
+app.use(express.json({limit: '100mb'}))
 app.use(express.urlencoded({
     extended: true
 }))

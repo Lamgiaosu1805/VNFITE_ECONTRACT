7bc474d8f80276c404a1f2384a36fab959753e3f
Lamgs <nghiemkhaclam185.mta@gmail.com>
2025-06-04 18:33:32 +0700

    change connect redis

--- src/config/connectRedis.js
+++ src/config/connectRedis.js
@@ -1,11 +1,13 @@
 const Redis = require("ioredis");
 
 const redis = new Redis({
-    host: "127.0.0.1", // Địa chỉ Redis server
-    port: 6379, // Cổng Redis (mặc định)
-  });
-  
-  redis.on("connect", () => console.log("🔗 Kết nối Redis thành công!"));
-  redis.on("error", (err) => console.error("❌ Lỗi Redis:", err));
-  
-  module.exports = redis;
+  host: process.env.REDIS_HOST,
+  port: 6379,
+  password: process.env.REDIS_PASSWORD,
+  db: 0,
+});
+
+redis.on("connect", () => console.log("🔗 Kết nối Redis thành công!"));
+redis.on("error", (err) => console.error("❌ Lỗi Redis:", err));
+
+module.exports = redis;
